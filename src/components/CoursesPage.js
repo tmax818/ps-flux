e073c5a1d4d451baa119c97de83ebb69b5ef1b39
tmax818 <tylermaxwell661@gmail.com>
2020-09-29 15:56:56 -0700

    constructor syntax

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+    };
+  }
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+export default CoursesPage;
