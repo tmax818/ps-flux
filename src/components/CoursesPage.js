44a046c70ef40987c6e3a51d0a46337355f59833
tmax818 <tylermaxwell661@gmail.com>
2020-09-29 16:07:56 -0700

    make table

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -12,7 +12,31 @@ class CoursesPage extends React.Component {
 
   render() {
     console.log(this.state);
-    return <h2>Courses</h2>;
+    return (
+      <div>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
