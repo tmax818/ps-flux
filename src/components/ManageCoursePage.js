c5b87042ab50a9d6eb2d252070a2a34dd28b4fff
tmax818 <tylermaxwell661@gmail.com>
2020-09-30 12:55:40 -0700

    fin

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = (props) => {
   console.log(props);
   return (
     <div>
       <h2>Manage Course</h2>
+      <Prompt when={true} message="what???" />
       {props.match.params.slug}
     </div>
   );
